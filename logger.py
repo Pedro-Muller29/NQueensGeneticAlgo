bb777be334c69d871fcce7e89b22cba94eed1c70
Pedro Mezacasa Muller <pedro.mezzacasa@gmail.com>
2024-07-03 11:31:01 -0300

    added a logger class to handle verbosity and prints in general

---
+++ logger.py
@@ -0,0 +1,21 @@
+from sliders import *
+
+class Logger:
+    def __init__(self):
+        self.verbose = VERBOSE
+
+    def generation_started(self, generation_number):
+        if self.verbose:
+            print("\n=============================================")
+            print(f"Generation Number: {generation_number}")
+
+    def generation_child(self, child_number, child, child_eval):
+        if self.verbose:
+            print(f"{child_number} - {format(child, '024b')}, eval -> {child_eval}")
+    
+    def found_winner(self, generation_number, child):
+        if self.verbose:
+            print("\n\n\n=============================================")
+            print("Found Answer: ")
+        print(f"{format(child, '024b')}")
+        print(f"\nTook: {generation_number} Generations")
--- main.py
+++ main.py
@@ -1,28 +1,32 @@
 from evaluation import *
 from population import *
+from logger import Logger
 
 def main():
     generation = generateStartingPopulation()
+    logger = Logger()
     
     generation_number = 0
     while True:
         generation_number += 1
-        print("\n=============================================")
-        print(f"Generation Number: {generation_number}")
+
+        logger.generation_started(generation_number)
+
+        ranked_generation = {}
+
         for n in range(1, len(generation) + 1):
             child = generation[n-1]
             child_eval = evaluate(child)
 
-            print(f"{n} - {format(child, '024b')}, eval -> {child_eval}")
+            logger.generation_child(n, child, child_eval)
 
             if child_eval == 0:
-                print("\n\n\n=============================================")
-                print("Found Answer: ")
-                print(f"{format(child, '024b')}")
-                print(f"\nTook: {generation_number} Generations")
+                logger.found_winner(generation_number, child)
                 return
             
-            
+            ranked_generation[child] = child_eval
+
+
 
 if __name__ == "__main__":
     main()
